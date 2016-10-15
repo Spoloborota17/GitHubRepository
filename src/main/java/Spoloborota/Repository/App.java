4287687ad8bc839b63b908beded659689a0f37a7
Ivan <epetuk@list.ru>
2016-10-15 15:06:29 +0300

    Прототип

---
+++ src/main/java/Spoloborota/Repository/App.java
@@ -0,0 +1,14 @@
+package Spoloborota.Repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println(" А ты что за файл? Я уже совсем запутался :(");
+    }
+}
